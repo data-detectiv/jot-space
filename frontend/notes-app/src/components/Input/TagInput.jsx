731c2f88442134b58514e22f5523e26298111677
Foster Oppong <oppongyaw049@gmail.com>
2025-06-30 01:19:49 -0400

    tags input

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { MdAdd } from "react-icons/md";
+import { MdAdd, MdClose } from "react-icons/md";
 
 const TagInput = ({ tags, setTags }) => {
   const [inputValue, setInputValue] = useState("");
@@ -16,16 +16,36 @@ const TagInput = ({ tags, setTags }) => {
   };
 
   const handleKeyDown = (e) => {
-    addNewTag();
+    if (e.key === "Enter") {
+        addNewTag();
+    }
   }
+
+  const handleRemoveTag = (tagToRemove) => {
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+  };
 
   return (
     <div>
+        {tags?.length > 0 && (
+            <div className="flex items-center gap-2 flex-wrap mt-2">
+                {tags.map((tag, index) => (
+                    <span key={index} className="flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded">
+                        #{tag} 
+                        <button onClick={() => {handleRemoveTag(tag)}}>
+                            <MdClose />
+                        </button>
+                    </span>
+                ))}
+            </div>
+        )}
+        
         <div className="flex items-center gap-4 mt-3">
             <input 
                 type="text" 
                 className="text-sm bg-transparent border px-3 py-2 rounded" 
                 placeholder="Add tags" 
+                value={inputValue}
                 onChange={handleInputChange}
                 onKeyDown={handleKeyDown}
             />
--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react"
 import TagInput from "../../components/Input/TagInput"
 
 const AddEditNotes = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
   return (
     <div>
         <div className="flex flex-col gap-2">
@@ -9,6 +14,8 @@ const AddEditNotes = () => {
                 type="text" 
                 className="text-2xl text-slate-950 outline-none"
                 placeholder="Go To Gym At 5"
+                value={title}
+                onChange={({ target }) => setTitle(target.value)}
             />
         </div>
         <div className="flex flex-col gap-2 mt-4">
@@ -18,11 +25,16 @@ const AddEditNotes = () => {
                 className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
                 placeholder="Content"
                 rows={10}
+                value={content}
+                onChange={({ target }) => setContent(target.value)}
             />
         </div>
         <div className="mt-3">
             <label className="input-label">TAGS</label>
-            <TagInput />
+            <TagInput 
+                tags={tags}
+                setTags={setTags}
+            />
         </div>
         <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>ADD</button>
     </div>
