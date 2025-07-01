912e7351da0a3594040fa7d275e928f2d83bc363
Foster Oppong <oppongyaw049@gmail.com>
2025-06-30 23:25:23 -0400

    jwt authentication

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const config = require('./config.json')
+const mongoose = require('mongoose');
+mongoose.connect(config.connectionString);
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+const { authenticateToken } = require('./utilities');
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.get("/", (req, res) => {
+    res.json({data: "hello"});
+});
+
+// create an account
+app.post("/create-account", async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    if (!fullName) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Full Name is required" });
+    }
+
+    if (!email) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Email is required" });
+    }
+
+    if (!password) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Password is required" })
+    }
+})
+
+app.listen(8000);
+
+module.exports = app;
